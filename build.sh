441f299d531fdc0d1089418e56fc96336e55c368
supermarioyl <leiyu.cs@outlook.com>
2024-02-20 21:08:03 +0800

    发布信息变更

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.0.11
+VERSION=1.0.13
 
 JAVA_HOME=$JAVA_11_HOME mvn versions:set -DnewVersion=$VERSION
 JAVA_HOME=$JAVA_11_HOME mvn --batch-mode clean install -DskipTests source:jar -Dmaven.javadoc.skip=true
